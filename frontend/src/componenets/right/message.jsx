4489d65bb0b2a0c8fb89ee7510d510ea19d4d243
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-12 12:45:29 +0530

    delete

--- frontend/src/componenets/right/message.jsx
+++ frontend/src/componenets/right/message.jsx
@@ -26,7 +26,7 @@ function Message() {
     if (!msgId) return;
 
     try {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}/${msgId}`);
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/Message/delete/${msgId}`);
 
       if (response.status === 200) {
         // ✅ Show toast
