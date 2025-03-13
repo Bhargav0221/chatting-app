43a1ea2b02c5a9a82e6a56c735b344abbdf8e801
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-13 16:50:36 +0530

    acasdcas

--- frontend/src/context/usedeletesocket.jsx
+++ frontend/src/context/usedeletesocket.jsx
@@ -3,7 +3,7 @@ import { usecontext } from './socketcontext'
 import useConversation from '../statemanage/userconversation'
 import Usegetmessage from './Usegetmessage';
 function usedeletesocket() {
-    const{message,setmessage} =useConversation()
+    const{message,setmessage} =Usegetmessage()
     const{socket}=usecontext();
     useEffect(()=>{
         if(!socket) return;
