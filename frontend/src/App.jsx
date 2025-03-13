43e0a6c5299eee5e05d588d64a50b23bfa345edd
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-13 20:11:26 +0530

    aewfasdf

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,38 @@
-import React from 'react';
-import Left from './components/left/left';
-import Right from './components/right/right';
-import Signup from './components/signup';
-import Login from './components/login';
-import { useauth } from './context/authcontext';
-import { Navigate, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import Left from './componenets/left/left'
+import Right from './componenets/right/right'
+import Logout from './componenets/left/logout'
+import Signup from './componenets/signup'
+import Login from './componenets/login'
+import { useauth } from './context/authcontext'
+import { Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 
+
+
 function App() {
-  const [user] = useauth();
-
-  console.log("user is made ", user);
-
+  const [ user, setuser ] = useauth();
+   
+  
+  console.log("user is made ",user);
   return (
-    <>
+<>
+  
       <Routes>
-        <Route
-          path="/"
-          element={
-            user ? (
-              <div className="flex flex-col md:flex-row h-screen w-full">
-                <Left />
-                <Right />
-              </div>
-            ) : (
-              <Navigate to="/signup" />
-            )
-          }
-        />
-        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
-        <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
+        <Route path="/" element={user ?( <div className='flex h-screen'>
+          <Logout />
+          <Left />
+          <Right />
+        </div>) : (<Navigate to={"/signup"}/>)} />
+        <Route path="/login" element={user?<Navigate to={"/"}/>:<Login/>}/>
+        <Route path="/signup" element={user?<Navigate to={"/"}/>:<Signup/>}/>
       </Routes>
       <ToastContainer />
-    </>
-  );
+</>
+  
+  )
 }
 
-export default App;
+export default App
+
