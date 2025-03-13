20e3217a456b68203bc57e61161d77f1ae1c71d0
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-13 20:00:56 +0530

    knk

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           path="/"
           element={
             user ? (
-              <div className="flex ">
+              <div className="flex flex-col md:flex-row h-screen w-full">
                 <Left />
                 <Right />
               </div>
