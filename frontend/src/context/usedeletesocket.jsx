4e46f1ce682cd0ce0edb5c491251b1113abb6a05
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-13 16:35:06 +0530

    xzcvzsd

--- frontend/src/context/usedeletesocket.jsx
+++ frontend/src/context/usedeletesocket.jsx
@@ -8,7 +8,7 @@ function usedeletesocket() {
     useEffect(()=>{
         if(!socket) return;
         const inlineHandler = (msgId) => {
-          const updatedMessages = message.filter((msg) => msg._id !== msgId);\
+          const updatedMessages = message.filter((msg) => msg._id !== msgId);
           console.log("deleted message to reciever side",updatedMessages);
           
           setmessage([...updatedMessages])
