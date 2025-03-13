a62f61acb50e315033cd1045ebb85a3691053296
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-13 18:10:55 +0530

    xdgzdg

--- frontend/src/componenets/left/search.jsx
+++ frontend/src/componenets/left/search.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 function search() {
   const [search, setSearch] = useState("");
   const [allUsers] = usegetalluser();
-  const { setselectedconversation} = useConversation();
+  const { setselectedconversation } = useConversation();
   console.log(allUsers);
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -25,12 +25,18 @@ function search() {
 
 
     <div className='px-6 py-3'>
-      <form  onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div className='flex space-x-3 h-[10vh]'>
-         
-            <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)}className="grow text-black" placeholder="Search" />
 
-        
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search users..."
+            className="w-full md:w-[80%] px-4 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 bg-white text-black shadow-sm transition duration-200"
+          />
+
+
           <button     >
             <IoSearch className='text-5xl p-2  hover:bg-gray-600 rounded-lg duration-300' />
           </button>
