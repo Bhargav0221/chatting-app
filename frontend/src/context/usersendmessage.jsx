ebfa028c67cd83d59d4e706fe9d18016fa6bbd1d
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-13 16:09:06 +0530

    asdfasdfs

--- frontend/src/context/usersendmessage.jsx
+++ frontend/src/context/usersendmessage.jsx
@@ -11,7 +11,7 @@ const useSendMessage = () => {
     try {
       const token = localStorage.getItem("token");
       const res = await axios.post(
-        `http://localhost:5000/Message/send/${selectedconversation._id}`,
+        `${import.meta.env.VITE_API_URL}/Message/send/${selectedconversation._id}`,
         {
           message: nemessage,
         },
