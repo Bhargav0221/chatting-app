9a9c4a607511f08529632fcfd24172bc0f2b1677
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-13 16:42:13 +0530

    ddsvzsdvzsv

--- frontend/src/statemanage/userconversation.jsx
+++ frontend/src/statemanage/userconversation.jsx
@@ -4,7 +4,7 @@
     selectedconversation:null,
     setselectedconversation: (selectedconversation) => set({selectedconversation}),
     message:[],
-    setmessage: (message = []) => set({ message: message || [] }) 
+    setmessage: (message) => set(message) 
   
   }));
   export default useConversation
