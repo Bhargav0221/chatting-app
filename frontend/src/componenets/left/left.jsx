d325f99650605d559f9c02b1c885e8106ced1f15
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-16 16:00:56 +0530

    bnk;bb;

--- frontend/src/componenets/left/left.jsx
+++ frontend/src/componenets/left/left.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Search from './search';
 import User from './user';
+import Logout from './logout';
 
 function Left() {
   return (
-    <div className="w-full md:w-[35%] lg:w-[30%] h-screen overflow-y-auto bg-black text-white border-r border-gray-700">
-      <Search />
-      <hr className="border-gray-600" />
-      <User />
+    <div className="flex flex-col h-screen w-full md:w-[35%] lg:w-[30%] bg-black text-white border-r border-gray-700">
+      
+      <div className="flex-1 overflow-y-auto">
+        <Search />
+        <hr className="border-gray-600" />
+        <User />
+      </div>
+
+     
+      <div className="border-t border-gray-700 p-3">
+        <Logout />
+      </div>
     </div>
   );
 }
