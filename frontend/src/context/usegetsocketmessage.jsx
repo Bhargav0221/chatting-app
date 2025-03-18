6413e4090de965a77bda0318406066298456270c
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-18 23:22:37 +0530

    jokjk

--- frontend/src/context/usegetsocketmessage.jsx
+++ frontend/src/context/usegetsocketmessage.jsx
@@ -5,17 +5,18 @@ import useConversation from '../statemanage/userconversation';
 const usegetsocketmessage = () => {
     const { message, setmessage } = useConversation();
     const { socket } = usecontext();
- const {selectedconversation}=useConversation()
 
     useEffect(() => {
         const handleNewMessage = (newmessage) => {
-            if (
-                selectedconversation &&
-                newmessage.reciever === selectedconversation._id
-            ) {
-                setmessage((prev) => [...prev, newmessage]);
+            if (message.length > 0) {
+                setmessage([...message, newmessage]);
             }
-          
+            else {
+                setmessage([newmessage]
+
+
+                )
+            }
         };
 
         socket.on("newmessage", handleNewMessage);
