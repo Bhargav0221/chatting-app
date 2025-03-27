76baf70d2e896a1b662897edcaf08159f5252b8b
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-27 18:32:05 +0530

    sxcsdc

--- backend/routes/otp.route.js
+++ backend/routes/otp.route.js
@@ -3,5 +3,5 @@ import { sendotp } from "../controller/otp.controller.js";
 import { verifyotp } from "../controller/otp.controller.js";
 const router=express.Router();
 router.post("/send-otp",sendotp);
-router.get("/verify-otp",verifyotp);
+router.post("/verify-otp",verifyotp);
 export default router;
