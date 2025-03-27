ce9a748b95cc7066b46363750cb91de1315ed41c
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-27 16:32:30 +0530

    jl'jp

--- frontend/src/componenets/otp.jsx
+++ frontend/src/componenets/otp.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useauth } from "../context/authcontext";
 import { useNavigate } from "react-router-dom";
-import { response } from "express";
+
 const OTPVerification = () => {
      const [user, setuser] = useauth();
   const navigate=useNavigate();
