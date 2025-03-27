10997de600cb684e15428f23a037f0e5695d0db6
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-27 19:15:57 +0530

    nk'nk'

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,11 @@
 
     app.use(express.json());    
     app.use(cors({
-           origin: "*",
+        origin: "https://chatting-app-topaz.vercel.app", 
            methods: ["GET", "POST", "PUT", "DELETE"],
 
-        credentials: true
-    }));
+        edentials: true
+    }));cr
     app.use(cookieParser());
 
     try{
