f7ddb8d45b07652a2c28ddd14b5d37b20d7a1048
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-27 15:45:58 +0530

    ascasf

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,7 +40,8 @@ function App() {
       )
     }
   />
-  <Route path="/otp" element={OTPVerification}/>
+<Route path="/otp" element={<OTPVerification />} />
+
   <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
   <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
 </Routes>
