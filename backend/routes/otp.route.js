f13406840dd12ed1ace1300fac06cb9f03498a41
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-27 16:18:05 +0530

    n,m/nljn

--- backend/routes/otp.route.js
+++ backend/routes/otp.route.js
@@ -4,3 +4,4 @@ import { verifyotp } from "../controller/otp.controller.js";
 const router=express.Router();
 router.post("/send-otp",sendotp);
 router.get("/verify-otp",verifyotp);
+export default router;
