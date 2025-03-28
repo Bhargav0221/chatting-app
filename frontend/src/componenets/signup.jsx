6a50f4127114ac4d02aa65e641f7667ed3f74471
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-03-28 11:20:23 +0530

    erfr

--- frontend/src/componenets/signup.jsx
+++ frontend/src/componenets/signup.jsx
@@ -26,7 +26,7 @@ function Signup() {
         }
         console.log("Form submitted", formdata);
         try {
-            if(!formdata.email.includes("@gmail.com")||!formdata.email.includes("@yahoo.com")||!formdata.email.includes("@chitkara.edu.in"))
+            if(!formdata.email.includes("@gmail.com")&&!formdata.email.includes("@yahoo.com")&&!formdata.email.includes("@chitkara.edu.in"))
             {
                 toast("Enter Valid email");
                 return ;
