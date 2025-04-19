1288a50eb82ad76661e7f7344fef160fcbed009e
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-04-19 19:30:03 +0530

    dsfcdsc

--- frontend/src/componenets/otp.jsx
+++ frontend/src/componenets/otp.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useauth } from "../context/authcontext";
+import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
 const OTPVerification = () => {
