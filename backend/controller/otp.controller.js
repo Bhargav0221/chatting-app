eb3c129716c6206769c44dc34dd464fdab8361ec
Bhargav0221 <122959959+Bhargav0221@users.noreply.github.com>
2025-09-25 20:38:19 +0530

    dfsd

--- backend/controller/otp.controller.js
+++ backend/controller/otp.controller.js
@@ -17,7 +17,7 @@ export const sendotp=async(req,res)=>{
         service: "gmail",
         auth: {
             user: "singlabhargav2004@gmail.com",
-            pass: "usat eiwy dhhn obsl"
+            pass: "qhlo tats seiw llae"
         },
     })
         const mailoptions={
